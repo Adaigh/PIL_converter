83693700852c63e1fe539b2ebbb1d0c104b7a2ee
Adaigh <adaigh@asu.edu>
2024-08-15 14:48:17 -0700

    Updated the layout and elements of the GUI for cleaner look

--- src/gui.py
+++ src/gui.py
@@ -1,50 +1,57 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilenames
+from converter import convert_images
 
-formats = ['.jpg', '.jpeg', '.png', '.bmp', '.blp', '.dds', '.dib', '.eps', '.gif', '.icns', '.ico', '.im', '.msp', '.pcx', '.pfm', '.ppm', '.tga', '.tiff', '.webp']
+
+formats = ['.jpg', '.jpeg', '.png', '.bmp', '.heic', '.heif', '.blp', '.dds', '.dib', '.eps', '.gif', '.icns', '.ico', '.im', '.msp', '.pcx', '.pfm', '.ppm', '.tga', '.tiff', '.webp']
+types = [("Image file", format) for format in formats]
 d_font = font=('Arial', 20, "bold")
+paths = []
 
 def generate_program_window():
 
-    paths = []
-
     def pickFiles():
-        paths = [file for file in askopenfilenames()]
+        global paths
+        paths = [file for file in askopenfilenames(parent=window, title="Select the images you want to convert", filetypes=types)]
         new_message = ''
         for p in paths:
             new_message += p.split('/')[-1] + '\n'
-        message['text'] = new_message
-        message['font'] = ('Arial', 12)
+        if new_message != '':
+            message['text'] = new_message
 
     def myclick():
-        message = "Hello, " + entry.get()
-        label = Label(frame, text = message, font = ("Times New Roman", 14, "italic"))
-        entry.delete(0,'end')
-        label.pack(pady=30)
+        global paths
+        message['text'] = 'Converting imgage(s).'
+        convert_images(paths, selection.get())
+        paths = []
+        message['text'] = 'Finished converting image(s).'
 
     # Create frame instance
     window = Tk()
 
     # Set frame geometry
-    window.geometry("750x750")
+    window.geometry("750x450")
 
-    frame = LabelFrame(window, width=650, height=650, bd=0, background='black').grid(column=0, row=0, columnspan=2, rowspan=4, padx=50, pady=50)
+    frame = LabelFrame(window, width=650, height=350, bd=0, background='black')
+    frame.grid(column=0, row=0, columnspan=2, rowspan=4, padx=50, pady=50)
 
-    Label(frame, text="Python-based image converter", font=d_font, background='black', foreground='#5bb963').grid(column=0, row=0, columnspan=2)
-    message = Label(frame, text="No files selected", font=d_font, background='black', foreground='#5bb963')
-    message.grid(column=0, row=1)
-    ttk.Button(frame, text = "Select Images", command = pickFiles).grid(column=1, row=1)
-    Label(frame, text="Convert images to: ", font=d_font, background='black', foreground='#5bb963').grid(column=0, row=2)
-    ttk.Combobox(
+    Label(window, text="Python-based image converter", font=d_font, background='black', foreground='#5bb963').grid(column=0, row=0, columnspan=2)
+
+    Label(window, text="Convert images to: ", font=d_font, background='black', foreground='#5bb963').grid(column=0, row=1, sticky=E)
+    selection = ttk.Combobox(
         state='readonly',
         values=formats,
-        font=d_font,
-        background='black',
-        foreground='#5bb963'
-    ).grid(column=1, row=2)
+    )
+    selection.set('.jpg')
+    selection.grid(column=1, row=1, sticky=W)
 
-    ttk.Button(frame, text="Click", command= myclick).grid(column=0, row=3, columnspan=2, pady=20)
+    message = Label(window, text="No files selected", anchor='nw')
+    message.grid(column=0, row=2, columnspan=2, sticky='NSEW')
+
+    ttk.Button(window, text = "Select Images", command = pickFiles).grid(column=0, row=3, sticky='NSEW', padx=20, pady=20)
+
+    ttk.Button(window, text="Convert Images =>", command= myclick).grid(column=1, row=3, sticky='NSEW', padx=20, pady=20)
     window.configure(background='black')
     window.mainloop()
 
