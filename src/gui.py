31f3943e6fa4479355fa9375cf8edeafcd4c0f97
Adaigh <adaigh@asu.edu>
2024-08-30 15:34:41 -0700

    Adapting to converter changes and adding window updates on tail-recursive loop

--- src/gui.py
+++ src/gui.py
@@ -1,31 +1,51 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilenames
-from converter import convert_images
+from converter import RGBImageConverter, formats
+from dotenv import load_dotenv
+import os
 
 
-formats = ['.jpg', '.jpeg', '.png', '.bmp', '.heic', '.heif', '.blp', '.dds', '.dib', '.eps', '.gif', '.icns', '.ico', '.im', '.msp', '.pcx', '.pfm', '.ppm', '.tga', '.tiff', '.webp']
 types = [("Image file", format) for format in formats]
 d_font = font=('Arial', 20, "bold")
-paths = []
 
 def generate_program_window():
+
+    load_dotenv()
 
     def pickFiles():
         global paths
         paths = [file.strip() for file in askopenfilenames(parent=window, title="Select the images you want to convert", filetypes=types)]
         new_message = ''
         for p in paths:
-            new_message += p.split('/')[-1] + '\n'
+            new_message += p.split('/')[-1].strip() + '\n'
         if new_message != '':
-            message['text'] = new_message
+            message.config(text= new_message)
+
+    def tail_recusive_convert(remaining_paths):
+        global paths
+        global converter
+
+        if len(remaining_paths) == 0:
+            paths = []
+            del converter
+            message.config(text = 'Finished converting image(s).')
+            window.update()
+        else:
+            converter.convert_image(remaining_paths[0])
+            message.config(text = message['text'] + '.')
+            window.update()
+            tail_recusive_convert(remaining_paths[1:])
+        
 
     def myclick():
         global paths
-        message['text'] = 'Converting imgage(s).'
-        convert_images(paths, selection.get())
-        paths = []
-        message['text'] = 'Finished converting image(s).'
+        global converter
+
+        converter = RGBImageConverter(selection.get(), os.getenv('OUT_DIR'))
+        message.config(text = 'Converting image(s).')
+        window.update()
+        tail_recusive_convert(paths)
 
     # Create frame instance
     window = Tk()
@@ -46,12 +66,12 @@ def generate_program_window():
     selection.set('.jpg')
     selection.grid(column=1, row=1, sticky=W)
 
-    message = Label(window, text="No files selected", anchor='nw')
+    message = Label(window, text="No files selected", anchor='nw', justify='left')
     message.grid(column=0, row=2, columnspan=2, sticky='NSEW')
 
-    ttk.Button(window, text = "Select Images", command = pickFiles).grid(column=0, row=3, sticky='NSEW', padx=20, pady=20)
+    ttk.Button(window, text = "Select Image(s)", command = pickFiles).grid(column=0, row=3, sticky='NSEW', padx=20, pady=20)
 
-    ttk.Button(window, text="Convert Images =>", command= myclick).grid(column=1, row=3, sticky='NSEW', padx=20, pady=20)
+    ttk.Button(window, text="Convert Image(s) =>", command = myclick).grid(column=1, row=3, sticky='NSEW', padx=20, pady=20)
     window.configure(background='black')
     window.mainloop()
 
