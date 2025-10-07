3ccb4f5b90953626aa7bf58a5d931ba0e252f863
Adaigh <adaigh@asu.edu>
2025-10-07 10:40:20 -0700

    Updates to handle import errors

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from ui.gui import generate_program_window 
+
+if __name__ == "__main__":
+    generate_program_window()
--- src/ui/gui.py
+++ src/ui/gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from utils.converter import formats
-from functions import pick_files, start_conversion
+from .functions import pick_files, start_conversion
 
 
 d_font = font=('Arial', 20, "bold")
@@ -30,9 +30,9 @@ def generate_program_window():
     img_window_message = Label(main_window, text="No files selected", anchor='nw', justify='left')
     img_window_message.grid(column=0, row=2, columnspan=2, sticky='NSEW')
 
-    ttk.Button(main_window, text = "Select Image(s)", command = pick_files(main_window, img_window_message)).grid(column=0, row=3, sticky='NSEW', padx=20, pady=20)
+    ttk.Button(main_window, text = "Select Image(s)", command = lambda: pick_files(main_window, img_window_message)).grid(column=0, row=3, sticky='NSEW', padx=20, pady=20)
 
-    ttk.Button(main_window, text="Convert Image(s) =>", command = start_conversion(format_selection, main_window, img_window_message)).grid(column=1, row=3, sticky='NSEW', padx=20, pady=20)
+    ttk.Button(main_window, text="Convert Image(s) =>", command = lambda: start_conversion(format_selection, main_window, img_window_message)).grid(column=1, row=3, sticky='NSEW', padx=20, pady=20)
     main_window.configure(background='black')
     main_window.mainloop()
 
